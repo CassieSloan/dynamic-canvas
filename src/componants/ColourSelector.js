9543e337c12b781d7f1b3570fb48ea3afc3eb39a
Cassie <cassandra.sloan16@gmail.com>
2020-01-07 16:35:04 +1100

    finished drawing pad

--- src/componants/ColourSelector.js
+++ src/componants/ColourSelector.js
@@ -1,22 +1,16 @@
 import React, { Component } from "react";
 
 class ColourSelector extends Component {
-    state = { hex: this.props.hex }
-
-    onInputChange(event) { //on name of element name of event
-        console.log(event.target.value);
-    }
 
     render() {
-        const { hex } = this.state
+        const { hex, onColourSelectorChange } = this.props
 
         return(
             <input 
             type="color" 
             value={ hex } 
-            onChange={(event) => { 
-                this.setState({ hex: event.target.value})
-            }} />
+            onChange={onColourSelectorChange } 
+            />
         );
     }
 
